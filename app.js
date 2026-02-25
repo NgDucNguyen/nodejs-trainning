3aea245e2d16911e76886211e36cfe54cc305c8d
NgDucNguyen <nguyenducnguyen27122006@gmail.com>
2026-02-25 22:01:01 +0700

    express

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log("Hello world");
+const express = require("express");
+
+const app = express();
+
+const PORT = 808;
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
+
+app.get("/v1", (req, res) => {
+  res.send("Tao la Nguyen");
+});
+
+app.listen(PORT, () => {
+  console.log(`My app is rynning on port: ${PORT}`);
+});
