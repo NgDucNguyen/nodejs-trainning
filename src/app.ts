c80ff12c86c4b2c0d732b41e3af46684fb1a77c2
NgDucNguyen <nguyenducnguyen27122006@gmail.com>
2026-02-26 09:12:13 +0700

    env

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,11 @@
 // const express = require("express");
 import express from "express";
+import "dotenv/config";
 const app = express();
 
-const PORT = 808;
+const PORT = process.env.PORT || 808;
 app.get("/", (req, res) => {
-  res.send("Hello world update");
+  res.send("Hello world env");
 });
 
 app.get("/v1", (req, res) => {
@@ -13,4 +14,5 @@ app.get("/v1", (req, res) => {
 
 app.listen(PORT, () => {
   console.log(`My app is rynning on port: ${PORT}`);
+  console.log("env port: ", process.env.PORT);
 });
