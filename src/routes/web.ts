4daa13f14ed86b7ad65c24899e609561b7f45142
NgDucNguyen <nguyenducnguyen27122006@gmail.com>
2026-03-06 17:14:02 +0700

    Design giao dien admin

--- src/routes/web.ts
+++ src/routes/web.ts
@@ -7,6 +7,10 @@ import {
   getViewUser,
   postUpdateUser,
 } from "controllers/user.controller";
+import {
+  getAdminUserPage,
+  getDashboardPage,
+} from "controllers/admin/dashboard.controller";
 
 const rounter = express.Router();
 
@@ -18,6 +22,10 @@ const webRoutes = (app: Express) => {
   rounter.post("/handle-delete-user/:id", postDeleteUser);
   rounter.get("/handle-view-user/:id", getViewUser);
   rounter.post("/handle-update-user", postUpdateUser);
+
+  //admin routes
+  rounter.get("/admin", getDashboardPage);
+  rounter.get("/admin/user", getAdminUserPage);
 
   app.use("/", rounter);
 };
