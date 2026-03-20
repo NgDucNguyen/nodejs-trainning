841410c1b2b1f870425db6a737e28399f0cf5fdf
NgDucNguyen <nguyenducnguyen27122006@gmail.com>
2026-03-20 21:58:17 +0700

    validate + them moi product

--- src/controllers/admin/product.controller.ts
+++ src/controllers/admin/product.controller.ts
@@ -1,11 +1,63 @@
+import { error } from "console";
 import { Request, Response } from "express";
-
+import { createProduct } from "services/admin/product.service";
+import { ProductSchema, TProductSchema } from "src/validation/product.schema";
 const getAdminCreateProductPage = async (req: Request, res: Response) => {
-  return res.render("admin/product/create.ejs");
+  const errors = [];
+  const oldData = {
+    name: "",
+    price: "",
+    detailDesc: "",
+    shortDesc: "",
+    quantity: "",
+    factory: "",
+    target: "",
+  };
+  return res.render("admin/product/create.ejs", {
+    errors,
+    oldData,
+  });
 };
 
 const postAdminCreateProduct = async (req: Request, res: Response) => {
-  const { name } = req.body;
+  const { name, price, detailDesc, shortDesc, quantity, factory, target } =
+    req.body as TProductSchema;
+  const validate = ProductSchema.safeParse(req.body);
+
+  if (!validate.success) {
+    //error
+    const errorsZod = validate.error.issues;
+    const errors = errorsZod?.map(
+      (item) => `${item.message} (${item.path[0]})`,
+    );
+
+    const oldData = {
+      name,
+      price,
+      detailDesc,
+      shortDesc,
+      quantity,
+      factory,
+      target,
+    };
+    return res.render("admin/product/create.ejs", {
+      errors,
+      oldData,
+    });
+  }
+
+  //success
+  const image = req?.file?.filename ?? "";
+  await createProduct(
+    name,
+    +price,
+    detailDesc,
+    shortDesc,
+    +quantity,
+    factory,
+    target,
+    image,
+  );
   return res.redirect("/admin/product");
 };
 
