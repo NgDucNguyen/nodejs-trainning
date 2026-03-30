3c62b523fd863a60e81e8698b3b497a9e6cf7dfb
NgDucNguyen <nguyenducnguyen27122006@gmail.com>
2026-03-30 10:26:44 +0700

    chuc nang chi tiet gio hang

--- src/controllers/client/product.controller.ts
+++ src/controllers/client/product.controller.ts
@@ -1,6 +1,10 @@
 import { Request, Response } from "express";
 import { use } from "passport";
-import { addProductToCart, getProductById } from "services/client/item.service";
+import {
+  addProductToCart,
+  getProductById,
+  getProductInCart,
+} from "services/client/item.service";
 
 const getProductPage = async (req: Request, res: Response) => {
   const { id } = req.params;
@@ -24,8 +28,13 @@ const postAddProductToCart = async (req: Request, res: Response) => {
 const getCartPage = async (req: Request, res: Response) => {
   const user = req.user;
   if (!user) return res.redirect("/login");
+  const cartDetails = await getProductInCart(+user.id);
 
-  return res.render("client/product/cart");
+  const totalPrice = cartDetails
+    ?.map((item) => +item.price * item.quantity)
+    ?.reduce((a, b) => a + b, 0);
+
+  return res.render("client/product/cart", { cartDetails, totalPrice });
 };
 
 export { getProductPage, postAddProductToCart, getCartPage };
--- src/services/client/item.service.ts
+++ src/services/client/item.service.ts
@@ -74,4 +74,19 @@ const addProductToCart = async (
     });
   }
 };
-export { getProducts, getProductById, addProductToCart };
+
+const getProductInCart = async (userId: number) => {
+  const cart = await prisma.cart.findUnique({
+    where: { userId },
+  });
+
+  if (cart) {
+    const currentCartDetail = await prisma.cartDetail.findMany({
+      where: { cartId: cart.id },
+      include: { product: true },
+    });
+    return currentCartDetail;
+  }
+  return [];
+};
+export { getProducts, getProductById, addProductToCart, getProductInCart };
